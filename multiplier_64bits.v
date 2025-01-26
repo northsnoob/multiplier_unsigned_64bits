baf7364275168e3cd0ab1c73f9127110973c8821
northsnoob <122870075+northsnoob@users.noreply.github.com>
2025-01-26 14:01:24 +0800

    Update multiplier_64bits.v

--- multiplier_64bits.v
+++ multiplier_64bits.v
@@ -1,16 +1,18 @@
-module multiplier_64bits(
+module multiplier_64bits #(parameter  BITS = 64)(
 	input clk,reset_n,w_en,
-	input [63:0] a_in,b_in,
+	input [BITS-1:0] a_in,b_in,
 	output wire ok_flag,
-	output reg [127:0] product_out
+	output reg [BITS*2-1:0] product_out
 );
-reg [63:0] b_in_tmp;
-wire [127:0] adder_Ain;
-reg [127:0] shift_tmp;
-wire [127:0] adder_wire;
-reg [6:0] counter;
+
+reg [BITS-1:0] b_in_tmp;
+wire [BITS*2-1:0] adder_Ain;
+reg [BITS*2-1:0] shift_tmp;
+wire [BITS*2-1:0] adder_wire;
+reg [$clog2(BITS):0] counter;
 wire adder_flag;
 assign ok_flag = counter[6]; // complete multiplication with 64 clk
+
 
 always@(posedge clk)begin
 	if(w_en)begin
@@ -22,13 +24,15 @@ always@(posedge clk)begin
 end
 
 
+
 mux64to1 my_mux1(
 	.I(b_in_tmp),
 	.O(adder_flag),
-	.S(counter[5:0])
+	.S(counter[$clog2(BITS)-1:0])
 );
-assign adder_Ain = (adder_flag)?shift_tmp:128'd0;
-adder_128bits my_adder(
+assign adder_Ain = (adder_flag)?shift_tmp:0;
+adder_128bits #(.BITS(BITS*2)) my_adder
+	(
 	.A_in(adder_Ain),
 	.B_in(product_out),
 	.sum_out(adder_wire),
@@ -44,18 +48,16 @@ always@(posedge clk or negedge reset_n)begin
 			product_out<=adder_wire;
 			counter<=counter+1;
 		end
-		//end else begin
-		//	product_out<=product_out; // 
-		//end
 	end
 end
 endmodule
 
-module adder_128bits(
-	input [127:0] A_in,B_in,
-	output [127:0] sum_out,
+module adder_128bits #(parameter BITS = 128)(
+	input [BITS-1:0] A_in,B_in,
+	output [BITS-1:0] sum_out,
 	output carry_out
 );
+
 assign {carry_out,sum_out}=A_in+B_in;
 endmodule
 
